f274e480cc950cbec628f9a10643b7afb852efd5
glyn-stevens <glynst@gmail.com>
2024-12-01 17:44:24 +0000

    add init file and template for each day

---
+++ advent_of_code/template.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from advent_of_code import ASSETS_DIR
+
+
+def main():
+    print(f"Running script {Path(__file__).name}...")
+    ASSETS_DIR / ""
+
+
+if __name__ == "__main__":
+    main()
