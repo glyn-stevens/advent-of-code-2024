d7de93e4fef663137d97f7cddec9a64e784d6604
glyn-stevens <glynst@gmail.com>
2024-12-11 00:32:01 +0000

    update template

--- advent_of_code/template.py
+++ advent_of_code/template.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-from advent_of_code.utils import read_input, solve, test
+from advent_of_code.utils import read_input_stripped, solve, test
 
 
 def main():
     print(f"Running script {Path(__file__).name}...")
-    inputs = read_input("day_.txt")
-    sample_inputs = read_input("day__sample.txt")
+    inputs = read_input_stripped("day_.txt")
+    sample_inputs = read_input_stripped("day__sample.txt")
     test(sample_inputs, part_1, "Part 1 test", expected=0)
     solve(inputs, part_1, "Part 1")
 
