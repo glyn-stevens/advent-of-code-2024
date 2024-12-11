d9bd6c273fe2bccd37f4ba357e6d7dfe794b0c34
GlynStevensGen <glyn@generative.vision>
2024-12-11 22:40:02 +0000

    typo

--- tests/test_day_11.py
+++ tests/test_day_11.py
@@ -1,6 +1,6 @@
 from pytest import fixture
 from advent_of_code.day_11 import part_1, part_2, parse_inputs
-from advent_of_code.utils import read_input_strippepoetry env use  C:\Users\Glyn\AppData\Local\Programs\Python\Python312-32\python.exed
+from advent_of_code.utils import read_input_stripped
 
 
 @fixture
